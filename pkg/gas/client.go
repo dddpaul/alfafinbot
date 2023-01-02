3e052c769b58736fd6e55ca7d1c826b5fd0418e0
Pavel Derendyaev <dddpaul@gmail.com>
2023-01-02 14:59:12 +0300

    Parse GAS response into struct

--- pkg/gas/client.go
+++ pkg/gas/client.go
@@ -1,6 +1,7 @@
 package gas
 
 import (
+	"encoding/json"
 	"errors"
 	"io/ioutil"
 	"net/http"
@@ -19,6 +20,22 @@ type Client struct {
 	u *url.URL
 }
 
+type Status int64
+
+const (
+	OK    Status = 0
+	ERROR        = 1
+)
+
+type Response struct {
+	Status  Status `json:"status"`
+	Message string `json:"message"`
+}
+
+func (r *Response) isError() bool {
+	return r.Status != OK
+}
+
 func NewClient(rawURL string) *Client {
 	u, err := url.Parse(rawURL)
 	if err != nil {
@@ -33,43 +50,56 @@ func (c *Client) Add(p *purchases.Purchase) (string, error) {
 	params.Add("merchant", p.Merchant)
 	params.Add("price", strconv.FormatFloat(p.Price, 'f', 2, 64))
 
-	res, err := http.PostForm(c.u.String(), params)
+	resp, err := http.PostForm(c.u.String(), params)
 	if err != nil {
 		return "", err
 	}
 
-	data, err := ioutil.ReadAll(res.Body)
+	r, err := parse(resp)
 	if err != nil {
 		return "", err
 	}
-	res.Body.Close()
 
-	s := string(data)
-	if strings.Contains(s, ".errorMessage") {
-		return "", errors.New("GAS: " + after(s, "Error: "))
-	}
-
-	return s, nil
+	return r.Message, nil
 }
 
 func (c *Client) CurrentMonthSum() (string, error) {
-	res, err := http.Get(c.u.String())
+	resp, err := http.Get(c.u.String())
 	if err != nil {
 		return "", err
 	}
 
-	data, err := ioutil.ReadAll(res.Body)
+	r, err := parse(resp)
 	if err != nil {
 		return "", err
 	}
-	res.Body.Close()
+
+	return r.Message, nil
+}
+
+// Parse HTTP response from Google App Script
+func parse(resp *http.Response) (*Response, error) {
+	data, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	resp.Body.Close()
 
 	s := string(data)
 	if strings.Contains(s, ".errorMessage") {
-		return "", errors.New("GAS: " + after(s, "Error: "))
+		return nil, errors.New("GAS: " + after(s, "Error: "))
 	}
 
-	return s, nil
+	r := &Response{}
+	if err = json.Unmarshal(data, r); err != nil {
+		return nil, err
+	}
+
+	if r.isError() {
+		return nil, errors.New("GAS: " + r.Message)
+	}
+
+	return r, nil
 }
 
 // Get substring after a string
