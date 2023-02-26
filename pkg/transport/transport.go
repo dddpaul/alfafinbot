3ea6dd74e5d22fb1fe5cebdd2fba8c6436c3fd59
Pavel Derendyaev <dddpaul@gmail.com>
2023-02-26 18:45:27 +0300

    Use socks dialer from forward-proxy

--- pkg/transport/transport.go
+++ pkg/transport/transport.go
@@ -4,40 +4,15 @@ import (
 	"context"
 	"net/http"
 	"net/http/httptrace"
-	"net/url"
 	"time"
 
 	"github.com/dddpaul/alfafin-bot/pkg/logger"
-
-	"golang.org/x/net/proxy"
+	"github.com/dddpaul/alfafin-bot/pkg/proxy"
 )
 
 func NewSocksTransport(socks string) http.RoundTripper {
-	if len(socks) == 0 {
-		return http.DefaultTransport
-	}
-
-	u, err := url.Parse(socks)
-	if err != nil {
-		panic(err)
-	}
-
-	var auth *proxy.Auth
-	if u.User != nil {
-		auth = &proxy.Auth{
-			User: u.User.Username(),
-		}
-		if p, ok := u.User.Password(); ok {
-			auth.Password = p
-		}
-	}
-
-	dialer, err := proxy.SOCKS5("tcp", u.Host, auth, proxy.Direct)
-	if err != nil {
-		panic(err)
-	}
 	return &http.Transport{
-		Dial: dialer.Dial,
+		Dial: proxy.NewDialer(socks).Dial,
 	}
 }
 
